7fac24bfb9cd48ffd280091b61dc5a670a145d72
unknown <erickvillegasecheverria@gmail.com>
2021-01-27 21:22:52 -0800

    MVP

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
 const db = require("../data/dbConfig.js");
-
 const server = express();
+const helmet = require("helmet");
+const ServerRouter = require("./router");
 
 server.use(express.json());
+server.use(helmet());
+server.use("/api", ServerRouter);
 
 module.exports = server;
